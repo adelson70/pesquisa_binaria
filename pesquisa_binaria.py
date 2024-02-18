08a71f5530246fbc8a5728f5e3b5b9e3d7395371
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-02-17 21:24:52 -0300

    add fonts

--- pesquisa_binaria.py
+++ pesquisa_binaria.py
@@ -6,14 +6,24 @@ from random import choice
 # GUI
 janela = tk.Tk()
 janela.title('Pesquisa Binária')
-janela.geometry('270x250')
+janela.geometry('270x150')
 
-label_numero_sortido = tk.Label(janela, text='')
-label_numero_sortido.pack(pady=5)
+# Fonte números
+fonte_numero = Font(family="Helvetica", size=20, weight="bold")
+fonte_botao = Font(family="Helvetica", size=14, weight="bold")
+
+
+
+label_numero_sortido = tk.Label(janela, text='16', font=fonte_numero)
+label_numero_sortido.pack(pady=10)
 
 botao_numero_maior = tk.Button(janela, text='Meu Número é Maior')
-botao_numero_menor = tk.Button(janela, text='Meu Número é Menor')
-botao_acertou_numero = tk.Button(janela, text='Acertou!')
+botao_numero_maior.pack(pady=3)
 
+botao_numero_menor = tk.Button(janela, text='Meu Número é Menor')
+botao_numero_menor.pack(pady=3)
+
+botao_acertou_numero = tk.Button(janela, text='Acertou!')
+botao_acertou_numero.pack(pady=3)
 
 janela.mainloop()
