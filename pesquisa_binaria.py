ddb9870f71cbc03bdb2bb126d2bc90687f74324c
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-02-17 21:13:38 -0300

    add file

---
+++ pesquisa_binaria.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter.font import Font
+from random import choice
+
+janela = tk.Tk()
+janela.title('Pesquisa Binária')
+janela.geometry('270x250')
+
+label_numero_sortido = tk.Label(janela, text='')
+label_numero_sortido.pack(pady=5)
+
+botao_numero_maior = tk.Button()
+botao_numero_menor = tk.Button()
+botao_acertou_numero = tk.Button()
+
+
+janela.mainloop()
